9760e594e9fa5b0a2a51d48955bc0dc4aef1df83
Geogre Noise <george.noise.dev+github@gmail.com>
2026-06-12 20:19:32 +0300

    version

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -1,6 +1,11 @@
+<script lang="ts">
+  import { version } from "./../../../package.json";
+</script>
+
 <div class="about">
   <p>Russet is simple paint powered by Svelte and Tailwind.</p>
-  <p>Created by George Noise (VulpesDust)</p>
+  <p>Created by George Noise (VulpesDust).</p>
+  <p>v{version}</p>
 </div>
 
 <style>
